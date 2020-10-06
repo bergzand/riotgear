084714e707fc4b97bf1a51e0c91e77b83957af65
Koen Zandberg <koen@bergzand.net>
2020-10-06 21:35:24 +0200

    plugin/builder: Add docker and jobs support

--- plugins/builder/plugin.py
+++ plugins/builder/plugin.py
@@ -2,6 +2,8 @@ from riotgear.plugin import BasePlugin
 
 import subprocess
 import multiprocessing  # CPU count
+import os
+from pathlib import Path
 
 
 class BuildPlugin(BasePlugin):
@@ -13,17 +15,33 @@ class BuildPlugin(BasePlugin):
                  docker=False):
 
         self.board = board
-        self.application = application
+        if application is not None:
+            app_path = Path(application)
+        else:
+            app_path = Path.cwd()
 
+        if not self._is_application(app_path):
+            print("No RIOT application found")
+            return
+
+        self.application = app_path
         core_count = multiprocessing.cpu_count()
         self.jobs = jobs if jobs is not None else core_count
         self.use_docker = docker
 
+    def _is_application(self, path):
+        makefile = path / "Makefile"
+        return makefile.is_file()
+
     def entry(self):
-        call = ['make', 'all', 'BOARD={}'.format(self.board)]
-        call.append("-j{}".format(self.jobs))
+        call_args = ['BOARD={}'.format(self.board)]
+        call_args.append("-j{}".format(self.jobs))
         if self.use_docker:
-            call.append("BUILD_IN_DOCKER=1")
-            call.append("DOCKER_MAKE_ARGS=-j{}".format(self.jobs))
-        print(call)
+            call_args.append("BUILD_IN_DOCKER=1")
+            call_args.append("DOCKER_MAKE_ARGS=-j{}".format(self.jobs))
+        self.build(call_args)
+
+    def build(self, call_args):
+        call = ['make', 'all']
+        call.extend(call_args)
         subprocess.call(call, cwd=self.application)
